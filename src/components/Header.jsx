f69b111852c96dbdb57101410c3263ed8a952b76
Olayinka-2 <olayinkaisrael802@gmail.com>
2025-03-03 05:24:21 +0100

    Revert "Add style to hero button"
    This reverts commit 3a0cf929f023e49b50b5c086f2efa2701cd2c678.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
    const location = useLocation();
 
    const homeIsActive = location.pathname === '/' ? 'active' : '';
-
+   console.log(homeIsActive);
    const shopIsActive = location.pathname === '/shopping' && 'active';
 
    return (
