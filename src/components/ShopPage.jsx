6233ded0e430d81d502cd436dd197d51872ff65f
Olayinka-2 <olayinkaisrael802@gmail.com>
2025-03-03 06:33:48 +0100

    Add the product designs to the page

--- src/components/ShopPage.jsx
+++ src/components/ShopPage.jsx
@@ -1,10 +1,51 @@
 import Header from "./Header";
+import image from './background.jpg';
 
 export default function Shop() {
    return (
       <>
          <Header />
-         <h1>Shop Page</h1>
+         <main>
+            <section className = "hero-section">
+               <div className="product-header">
+                  Our products available
+               </div>
+               <div className="product-container">
+                  <div className="products">
+                  <div className="product-card">
+                     <div className="product-image">
+                        <img src={image} alt="" />
+                     </div>
+                     <div className="product-description">
+                        <p>School bag</p>
+                        <p>price taeg</p>
+                        <p>Dsicount available</p>
+                     </div>
+                  </div>
+                  <div className="product-card">
+                     <div className="product-image">
+                        <img src={image} alt="" />
+                     </div>
+                     <div className="product-description">
+                        <p>School bag</p>
+                        <p>price taeg</p>
+                        <p>Dsicount available</p>
+                     </div>
+                  </div>
+                  <div className="product-card">
+                     <div className="product-image">
+                        <img src={image} alt="" />
+                     </div>
+                     <div className="product-description">
+                        <p>School bag</p>
+                        <p>price taeg</p>
+                        <p>Dsicount available</p>
+                     </div>
+                  </div>
+                  </div>
+               </div>
+            </section>
+         </main>
       </>
    )
 }
